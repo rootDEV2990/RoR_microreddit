8c25b74aa8d2bd519aa546c40442a98236533214
rootDEV2990 <website.dev@icloud.com>
2020-09-17 10:58:10 -0500

    testing validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_many :comments
+  #validates :title, presence: true, length: { minimum: 4, maximum: 10 }
+  validates_length_of :title, minimum: 5, maximum: 10, presence: true
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
     has_many :posts
     has_many :comments
+    validates :name, presence: true
+    validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
 end
