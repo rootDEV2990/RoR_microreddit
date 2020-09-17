0ff02ae1bd23021c464df73df6a9749524dfbc67
rootDEV2990 <website.dev@icloud.com>
2020-09-17 13:02:44 -0500

    Tested comments model, and rubocop add

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-    has_many :posts, dependent: :delete_all
-    has_many :comments
-    validates :name, presence: true
-    validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
+  has_many :posts, dependent: :delete_all
+  has_many :comments
+  validates :name, presence: true
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
 end
